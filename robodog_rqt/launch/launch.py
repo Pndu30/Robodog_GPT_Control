14b5b220eb0a1c8389655962d588568977be1f0e
Pndu <121600932+Pndu30@users.noreply.github.com>
2025-07-09 17:48:39 +0700

    Initial commit

---
+++ robodog_rqt/launch/launch.py
@@ -0,0 +1,31 @@
+from launch import LaunchDescription
+from launch.actions import GroupAction
+from launch_ros.actions import Node, PushRosNamespace
+
+def generate_launch_description():
+    return LaunchDescription([
+        GroupAction([
+            PushRosNamespace("robodog"),
+
+            Node(
+                package="robodog_gpt",
+                executable="control",
+                name="control",
+                output='screen'
+            ),
+
+            Node(
+                package="robodog_gpt",
+                executable="gpt_node",
+                name="gpt_node",
+                output='screen'
+            ),
+
+            Node(
+                package='robodog_rqt',
+                executable='gui_node',
+                name='robodog_gui',
+                output='screen',
+            ),
+        ])
+    ])
